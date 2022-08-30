6eb8a02c4cabbabaa841517fb829a631b6346e6b
tijani <tijaniadewale33@gmail.com>
2022-08-30 14:03:36 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of n: ");
+            int valueOfn = int.Parse(Console.ReadLine());
+            string result = Class1.practice(valueOfn);
+
+            Console.WriteLine(result);
+        }       
+    } 
+}
